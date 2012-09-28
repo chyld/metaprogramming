c536f19294f874288dab702275b994e79521de0f
Chyld Medford <chyld.medford@gmail.com>
2012-09-28 01:04:37 -0400

    updated eigenclass

--- eigenclass.rb
+++ eigenclass.rb
@@ -1,16 +1,7 @@
-# the eigenclass instance method on Object will now be inherited by all classes
-class Object
-  def eigenclass
-    class << self
-      self
+class BasicObject
+  def ghost
+    class << self # going into the ghost class of the object self
+      self # now that we're in the ghost class return it to the caller
     end
   end
 end
-
-# displays the eigenclass for the string object
-s = String.new
-puts s.eigenclass
-
-# displays the eigenclass for the String class object
-puts String.eigenclass
-
